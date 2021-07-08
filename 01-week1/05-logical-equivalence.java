b57014e512e6b40da05e151f03cba1087ea23cfc
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-07-08 14:40:27 -0400

    Create 05-logical-equivalence.java

---
+++ 01-week1/05-logical-equivalence.java
@@ -0,0 +1,12 @@
+1.4 Logical equivalence
+Definition: the compound propositions p and q are said to be logically equivalent if p ↔ q is a tautology.
+Logical equivalence is denoted by ↔ and ≡
+  if p is true, q has to be true
+    if p is false, q has to be false
+A compound proposition is a tautology if the proposition is always true, regardless of the truth value of the individual propositions that occur in it. 
+A compound proposition is a contradiction if the proposition is always false, regardless of the truth value of the individual propositions that occur in it.
+
+Two compound propositions are said to be logically equivalent if they have the same truth value regardless of the truth values of their individual propositions. 
+If s and r are two compound propositions, the notation s ≡ r is used to indicate that r and s are logically equivalent. 
+For example, p and ¬¬p have the same truth value regardless of whether p is true or false, so p ≡ ¬¬p. 
+Propositions s and r are logically equivalent if and only if the proposition s ↔ r is a tautology.
