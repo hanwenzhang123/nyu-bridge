733ef51845c878e22c5b0d9d4befc62241756134
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-07-09 22:16:08 -0400

    Create 03-upper-case.cpp

---
+++ 02-week2/03-upper-case.cpp
@@ -0,0 +1,21 @@
+Convert to UPPER CASE
+Example: 
+please enter a lower case letter: t
+the upper case of t is T
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  char lowerCaseLetter, upperCaseLetter;
+  int offset;
+
+  cout<<"Please enter a lower case letter: "<<endl;
+  cin>>lowerCaseLetter;
+
+  offset = (int)(lowerCaseLetter - 'a');
+  upperCaseLetter = (char)('A' + offset);   //that will be uppercase letter
+  
+  cout<<"The upper case of "<<lowerCaseLetter<<" is "<<upperCaseLetter<<endl;
+  return 0;
+}
