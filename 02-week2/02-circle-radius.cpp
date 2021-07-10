e5c31e0c410dafe8bfb3d59e1276a0cd49cb25f8
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-07-09 21:45:54 -0400

    Create 02-circle-radius.cpp

---
+++ 02-week2/02-circle-radius.cpp
@@ -0,0 +1,27 @@
+Area of a Circle
+example: 
+please enter the radius: 2.6
+the area of a circle with radius of 2.6 is 21.2372
+  
+Theorem:
+let c be a circle with a radius of length r.
+  we have (Area of c) = pie * r to the second power
+  
+
+#include <iostream>   //extension
+#include <cmath>
+using namespace std;
+
+int main() {
+  double radius, area;
+  
+  cout<<"please enter the radius: "<<endl;
+  cin>>daysTraveled;
+  
+  area = M_PI * (radius*radius);    //from the cmath library
+  
+  cout<<"the area of a circle with radius of "<<radius<<" is "<<area<<endl;
+  
+  return 0;
+}
+  
