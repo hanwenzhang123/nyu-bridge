3e180685c18a73b4daa32bc33b3f3cad995ae093
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-07-09 21:34:03 -0400

    Update and rename testing to 01-week-days.cpp

---
+++ 02-week2/01-week-days.cpp
@@ -0,0 +1,28 @@
+Weeks and Days
+if 19 days, there will be 2 weeks and 5 days
+19 divide 7 = 2 R 5
+
+19/7 = 2
+19%7 = 5
+  
+  
+//Code
+#include <iostream>
+using namespace std;
+
+const int DAYS_IN_A_WEEK = 7;   //for constance, we use upper cases and seperate words with underscore //7 is the number of the week
+int main() {
+  int daysTraveled;
+  int fullWeeks, remainingDays;
+  
+  cout<<"please enter the total days:"<<endl;
+  cin>>daysTraveled;
+  
+  fullWeeks = daysTraveled / DAYS_IN_A_WEEK;
+  remainingDays = daysTraveled % DAYS_IN_A_WEEK;  
+  
+  cout<<daysTraveled<<" days are "<<fullWeeks<<" weeks and "<<remainingDays<<" days."<<endl;
+  
+  return 0;
+}
+  
